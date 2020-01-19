f84b80fa1ad413678e18bfdae608074b825e33e2
MaticBernik <mb2551@student.uni-lj.si>
2020-01-20 00:45:48 +0100

    health, consul, logger

--- imagePortalFrontend/server_views.py
+++ imagePortalFrontend/server_views.py
@@ -209,3 +209,57 @@ def gui_upload(service, version):
         return redirect(upload_api, code=307)
 
     return render_template("upload.html")
+
+@app.route("/health/liveness")
+def liveness():
+    healthStatus = None
+    try:
+        if "consul_server" in app.config and app.config["consul_server"] is not None:
+            index = None
+            index, data = app.config["consul_server"].kv.get("frontend/alive", index=index)
+            if data is not None:
+                healthStatus = data["Value"]
+            else:
+                healthStatus = "true"
+        else:
+            healthStatus = "true"
+    except:
+        healthStatus = "false"        
+
+    if "false" in str(healthStatus).lower():
+        response = jsonify(
+        service_status="FAIL",
+        service_code=503)
+        return response, 503
+    else:
+        response = jsonify(
+        service_status="PASS",
+        service_code=200)
+        return response, 200
+    
+@app.route("/health/readiness")
+def readiness():
+    healthStatus = None
+    try:
+        if "consul_server" in app.config and app.config["consul_server"] is not None:
+            index = None
+            index, data = app.config["consul_server"].kv.get("frontend/ready", index=index)
+            if data is not None:
+                healthStatus = data["Value"]
+            else:
+                healthStatus = "true"
+        else:
+            healthStatus = "true"
+    except:
+        healthStatus = "false"         
+
+    if "false" in str(healthStatus).lower():
+        response = jsonify(
+        service_status="FAIL",
+        service_code=503)
+        return response, 503
+    else:
+        response = jsonify(
+        service_status="PASS",
+        service_code=200)
+        return response, 200    
